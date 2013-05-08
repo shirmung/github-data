3f2b0fa4bcb2cada3fa77a16958af32de83ed961
shirmung <shirmung@gmail.com>
2013-05-08 15:56:40 -0400

    adding data results

---
+++ data/sort.rb
@@ -0,0 +1,11 @@
+require 'csv'
+
+counts = Array.new(24, 0)
+
+CSV.foreach('results.csv') do |row|
+  if (row[0] != 'null' && row[1] != 'null')
+    counts[row[0][11..12].to_i] += 1
+  end
+end
+
+puts counts
