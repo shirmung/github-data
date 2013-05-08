c07e8f41219c2c06897134bca41edeab0d7ac42a
shirmung <shirmung@gmail.com>
2013-05-08 19:39:00 -0400

    cleaning up some CSS/JS

--- data/sort.rb
+++ data/sort.rb
@@ -3,9 +3,9 @@ require 'csv'
 counts = Array.new(24, 0)
 
 CSV.foreach('results.csv') do |row|
-  if (row[0] != 'null')
-    counts[row[0][11..12].to_i] += 1
-  end
+    if (row[0] != 'null')
+      counts[row[0][11..12].to_i] += 1
+    end
 end
 
 puts counts
---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(function() {
+    var colors = ['#053645', '#194654', '#2E5764', '#436873', '#587983', '#6D8992', '#829AA2',
+                  '#829AA2', '#6D8992', '#587983', '#436873', '#2E5764', '#194654', '#053645'];
+
+    var results = [184, 280, 202, 839, 228, 515, 147, 395, 344, 774, 297, 455, 629, 327, 437,
+                   1120, 436, 603, 944, 645, 990, 1838, 309, 1344];
+
+    $("#slider").slider({
+        step: 1,
+        max: 23,
+        value: 7,
+
+        change: function(event, ui) {
+            var colorIndex = Math.floor(ui.value/2);
+            var resultsIndex = ui.value;
+
+            $('body').animate({
+                backgroundColor: colors[colorIndex]
+            }, 1000);
+
+            var html = "";
+
+            for (var i = 0; i < Math.floor(results[resultsIndex]/15); i++) {
+                html += '<div class="computer"></div>';
+            }
+
+            var partial = ((results[resultsIndex] % 15) / 15) * 38;
+            html += '<div class="computer" style="width: ' + partial + 'px;"></div>';
+    
+            $('#results').hide().html(html).fadeIn(1000);
+        }
+    });
+
+    var colorIndex = Math.floor($("#slider").slider('value')/2);
+    var resultsIndex = $("#slider").slider('value');
+
+    $('body').animate({
+        backgroundColor: colors[colorIndex]
+    }, 1000);
+
+    var html = "";
+
+    for (var i = 0; i < Math.floor(results[resultsIndex]/15); i++) {
+        html += '<div class="computer"></div>';
+    }
+
+    var partial = ((results[resultsIndex] % 15) / 15) * 38;
+    html += '<div class="computer" style="width: ' + partial + 'px;"></div>';
+
+    $('#results').hide().html(html).fadeIn(1000);
+});
