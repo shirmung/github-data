8640ffb035e93b8c9ba8774a7f30864b29d8d524
shirmung <shirmung@gmail.com>
2013-05-08 19:09:08 -0400

    updating data

--- data/sort.rb
+++ data/sort.rb
@@ -3,7 +3,7 @@ require 'csv'
 counts = Array.new(24, 0)
 
 CSV.foreach('results.csv') do |row|
-  if (row[0] != 'null' && row[1] != 'null')
+  if (row[0] != 'null')
     counts[row[0][11..12].to_i] += 1
   end
 end
